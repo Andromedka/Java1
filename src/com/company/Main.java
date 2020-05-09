feecf3b2d6fb23e8ec4c8e8434c0487a964ee50e
Andromedka <androspace93@gmail.com>
2020-05-09 23:13:47 +0300

    my first code

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package src.com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int b = 7;
+
+        System.out.println( Number(5000) );
+    }
+
+    public static boolean Number(int k) {
+        int g;
+        g = k % 3;
+        if (g == 0)
+        return true;
+        else 
+        return false;
+        
+    }
+
+   
+}
